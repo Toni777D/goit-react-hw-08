4f83554e2eb52a897c730aeb197aab38e26b04ac
Toni777D <39744158+Toni777D@users.noreply.github.com>
2024-08-08 17:18:06 +0200

    hw-08.3

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -4,10 +4,13 @@ import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import {selectLoading} from "../../redux/contacts/selectors";
 import { fetchContacts } from "../../redux/contacts/operations";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import Contact from "../../components/Contact/Contact";
 
 
 export default function ContactsPage() {
     const dispatch = useDispatch();
+    const isLoggedIn = useSelector(selectIsLoggedIn)
     const loading = useSelector(selectLoading);
 
     useEffect(() => {
@@ -15,12 +18,15 @@ export default function ContactsPage() {
     }, [dispatch])
 
     return(
-        <div>
-            <h2>Your contacts</h2>
-            <ContactForm/>
-            <div>{loading && "Request in progress"}</div>
-            {error && <p>Error: {error}</p>}
-            <ContactList/>
+        <div>  
+           {isLoggedIn && ( 
+            <div>
+                <h2>Your contacts</h2>
+                <ContactForm/>
+                <div>{loading && "Request in progress"}</div>
+                <ContactList/>
+                <Contact/>
+           </div>)}
         </div>
     );
 }
